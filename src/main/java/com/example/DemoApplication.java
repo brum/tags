746c57a0e9a5caf6003740d354751646315ca553
Alexey Balakin <coolidea@ya.ru>
2017-02-04 12:07:52 +0300

    Exclude jmx autoconfig

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JmxAutoConfiguration.class})
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(DemoApplication.class);
+        app.run(args);
+    }
 }
