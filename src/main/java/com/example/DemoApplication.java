be751414d9221d580400d0dc92bd45f1a3813b2a
Alexey Balakin <coolidea@ya.ru>
2017-02-08 04:56:08 +0300

    use SpringApplicationBuilder

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,16 +1,18 @@
 package com.example;
 
 import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(exclude = {JmxAutoConfiguration.class})
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(DemoApplication.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+        new SpringApplicationBuilder(DemoApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .logStartupInfo(false)
+                .web(false)
+                .run(args);
     }
 }
