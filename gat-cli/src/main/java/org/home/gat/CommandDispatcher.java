2e56f5a357bed8b4ec4e99d3421f685bff2d1729
Alexey Balakin <coolidea@ya.ru>
2017-02-13 05:24:21 +0300

    add "shell" command

--- gat-cli/src/main/java/org/home/gat/CommandDispatcher.java
+++ gat-cli/src/main/java/org/home/gat/CommandDispatcher.java
@@ -1,8 +1,10 @@
 package org.home.gat;
 
 import io.airlift.airline.Cli;
+import io.airlift.airline.ParseException;
 import org.home.gat.command.GatCommand;
 import org.home.gat.command.HelpCommand;
+import org.home.gat.command.ShellCommand;
 import org.home.gat.command.TagListCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,31 +13,50 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CommandDispatcher implements CommandLineRunner, ApplicationContextAware {
 
     private static final Logger log = LoggerFactory.getLogger(CommandDispatcher.class);
 
+    private final Cli<GatCommand> gatDispatcher;
+
     private ApplicationContext applicationContext;
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
+    CommandDispatcher() {
 
-    @Override
-    public void run(String... args) throws Exception {
         Cli.CliBuilder<GatCommand> builder = Cli.<GatCommand>builder("gat")
                 .withDescription("The program for tagging files")
-                .withDefaultCommand(HelpCommand.class)
-                .withCommand(HelpCommand.class);
+                .withDefaultCommand(ShellCommand.class)
+                .withCommands(Arrays.asList(
+                        HelpCommand.class,
+                        ShellCommand.class
+                ));
 
         builder.withGroup("tags")
                 .withDescription("Manage tags")
                 .withDefaultCommand(TagListCommand.class)
                 .withCommand(TagListCommand.class);
 
-        Cli<GatCommand> gatDispatcher = builder.build();
+        gatDispatcher = builder.build();
+    }
 
-        gatDispatcher.parse(args).run(applicationContext);
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public void dispatchCommand(List<String> args) {
+        try {
+            gatDispatcher.parse(args).run(applicationContext);
+        } catch (ParseException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        dispatchCommand(Arrays.asList(args));
     }
 }
