22d80368682b4d7a06caece4fa9070d3a0de4139
Alexey Balakin <coolidea@ya.ru>
2017-02-20 06:33:59 +0300

    add tag store in memory db

--- gat-core/src/main/java/org/home/gat/tag/impl/InMemoryTagStore.java
+++ gat-core/src/main/java/org/home/gat/tag/impl/InMemoryTagStore.java
@@ -1,52 +1,19 @@
 package org.home.gat.tag.impl;
 
-import com.google.common.base.Strings;
-import org.home.gat.exception.TagNotFoundException;
 import org.home.gat.tag.api.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class InMemoryTagStore implements TagStore {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public List<Tag> list(String tagName) {
-        if (Strings.isNullOrEmpty(tagName)) {
-            return Arrays.asList(
-                    new Tag("tag1", true),
-                    new Tag("tag2", true),
-                    new Tag("tag11", false),
-                    new Tag("tag12", true),
-                    new Tag("tag121", false),
-                    new Tag("tag21", false)
-            );
-        }
-        switch (tagName) {
-            case "tag1": {
-                return Arrays.asList(
-                        new Tag("tag11", false),
-                        new Tag("tag12", true)
-                );
-            }
-            case "tag2": {
-                return Arrays.asList(
-                        new Tag("tag21", false)
-                );
-            }
-            case "tag12": {
-                return Arrays.asList(
-                        new Tag("tag121", false)
-                );
-            }
-            case "tag11":
-            case "tag121":
-            case "tag21": {
-                return Collections.emptyList();
-            }
-            default: {
-                throw new TagNotFoundException(tagName);
-            }
-        }
+        return jdbcTemplate.query("SELECT * FROM TAG", (rs, rowNum) ->
+                new Tag(rs.getString(2)));
     }
 }
